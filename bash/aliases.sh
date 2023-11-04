f30186676c085b4d13f42d351947a06a4a25f6d5
Bruce D'Arcus <bdarcus@gmail.com>
2023-11-04 11:33:04 -0400

    chore: exa -> eza
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,8 +1,7 @@
-alias ls='exa'
+alias ls='eza'
 alias l=ls
-alias ll='exa -l'
+alias ll='eza -l'
 alias grep='grep --color=auto'
 alias diff='diff --color=auto'
 alias open=xdg-open
 alias rm=trash
-
